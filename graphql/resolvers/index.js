4b9196f27bbf73ac894a603bcd299e5fd53939d9
robbiedv <robbie.c.devries@gmail.com>
2020-07-25 18:07:47 -0500

    GraphQL resolvers created

---
+++ graphql/resolvers/index.js
@@ -0,0 +1,33 @@
+
+cont Article = require("../../models/article")
+
+module.exports = {
+  articles: async () => {
+    try {
+      const articlesFetched = await Article.find()
+      return articlesFetched.map(article => {
+        return {
+          ...article._doc,
+          _id: article.id,
+          createdAt: new Date(article._doc.createdAt).toISOString(),
+        }
+      })
+    } catch (error) {
+      throw error
+    }
+  },
+
+  createArticle: async args => {
+    try {
+      const {title, body} = args.articles
+      const article = new Article({
+        title,
+        body,
+      })
+      const newArticle = await article.save()
+      return {...newArticle._doc, _id: newArticle.id}
+    } catch (error) {
+      throw error
+    }
+  },
+}
