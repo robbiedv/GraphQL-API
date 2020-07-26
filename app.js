413d572894daf72c8e8234f5e5ab708935d2da3d
robbiedv <robbie.c.devries@gmail.com>
2020-07-25 20:14:23 -0500

    env file created, app.js modified to connect to DB

--- app.js
+++ app.js
@@ -15,4 +15,11 @@ app.use(
   })
 )
 
-app.listen(3000, () => console.log("Server is running on localhost:3000"))
+const uri = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@graphql-api.8nv7g.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`
+const options = {useNewUrlParser: true, useUnifiedTopology: true}
+mongoose
+  .connect(uri, options)
+  .then(() => app.listen(3000, () => console.log("Server is running")))
+  .catch(error => {
+    throw error
+  })
