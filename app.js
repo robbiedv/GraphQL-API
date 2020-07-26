da1064b5333665a2ccece7713b27c78e419bb58f
robbiedv <robbie.c.devries@gmail.com>
2020-07-26 11:16:03 -0500

    refactoring

--- app.js
+++ app.js
@@ -6,9 +6,7 @@ const graphqlResolvers = require("./graphql/resolvers")
 
 const app = express()
 
-app.use(
-  "/graphql",
-  graphqlHttp({
+app.use('/graphql', graphqlHttp({
     schema: graphqlSchema,
     rootValue: graphqlResolvers,
     graphiql: true,
@@ -20,8 +18,7 @@ const options = {
   useNewUrlParser: true,
   useUnifiedTopology: true
 }
-mongoose
-  .connect(uri, options)
+mongoose.connect(uri, options)
   .then(() => app.listen(3000, console.log("Server is running")))
   .catch(error => {
     throw error
