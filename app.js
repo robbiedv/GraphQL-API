6e6db85aba919b633abb2d0cb659119c36f4f58d
robbiedv <robbie.c.devries@gmail.com>
2020-07-26 10:34:47 -0500

    required mongoose, refactoring

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-
 const express = require("express")
 const graphqlHttp = require("express-graphql")
+const mongoose = require("mongoose")
 const graphqlSchema = require("./graphql/schema")
 const graphqlResolvers = require("./graphql/resolvers")
 
-const app = express ()
+const app = express()
 
 app.use(
   "/graphql",
@@ -15,11 +15,14 @@ app.use(
   })
 )
 
-const uri = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@graphql-api.8nv7g.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`
-const options = {useNewUrlParser: true, useUnifiedTopology: true}
+const uri = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0-uox7n.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`
+const options = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}
 mongoose
   .connect(uri, options)
-  .then(() => app.listen(3000, () => console.log("Server is running")))
+  .then(() => app.listen(3000, console.log("Server is running")))
   .catch(error => {
     throw error
   })
