bc69a1544c444a193cf96894a5a47f9a14fc177f
robbiedv <robbie.c.devries@gmail.com>
2020-07-29 22:28:24 -0500

    mounted graphqlHTTP as route handler, still receiving error

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
-const express = require("express")
-const graphqlHttp = require("express-graphql")
-const mongoose = require("mongoose")
-const graphqlSchema = require("./graphql/schema")
-const graphqlResolvers = require("./graphql/resolvers")
+const express = require("express");
+const { graphqlHttp } = require("express-graphql");
+const mongoose = require("mongoose");
+const graphqlSchema = require("./graphql/schema");
+const graphqlResolvers = require("./graphql/resolvers");
 
-const app = express()
+const app = express();
 
 app.use('/graphql', graphqlHttp({
     schema: graphqlSchema,
     rootValue: graphqlResolvers,
     graphiql: true,
-  })
-)
+  }),
+);
 
 const uri = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0-uox7n.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`
 const options = {
